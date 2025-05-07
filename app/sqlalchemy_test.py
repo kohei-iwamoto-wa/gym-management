09d1d8c24eac63a62cf4128141e44c458b0ecfe8
iwamoto kohei <kohei.iwamoto@persol.co.jp>
2025-05-07 11:44:22 +0900

    aurora接続確認用データベース作成しました。

---
+++ app/sqlalchemy_test.py
@@ -0,0 +1,33 @@
+import os
+from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData
+from dotenv import load_dotenv
+load_dotenv()
+
+
+db_password = os.getenv("DB_PASSWORD")
+
+print("db_password", db_password)
+
+engine = create_engine(f'mysql+pymysql://addmin_user:{db_password}@local-aurora-mysql:3306/gym_db')
+
+metadata = MetaData()
+
+users = Table('users', metadata,
+              Column('id', Integer, primary_key=True),
+              Column('name', String(255)),
+              Column('age', Integer),
+              Column('email', String(255))
+              )
+
+metadata.create_all(engine)
+
+insert_stmt = users.insert().values(
+    name='Alice', age=25, email='alice@example.com')
+conn = engine.connect()
+conn.execute(insert_stmt)
+conn.commit()
+print("commit finish")
+select_stmt = users.select()
+result = conn.execute(select_stmt)
+for row in result:
+    print(row)
