5c4f8b1c462223d1ceb4a0cb13dcd4d5c360b337
kohei.iwamoto <kohei.iwamoto@persol.co.jp>
2025-05-09 15:01:21 +0900

    コードパイプライン修正

--- app/main.py
+++ app/main.py
@@ -23,6 +23,10 @@ table = dynamodb.Table(table_name)
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/aloha")
+def read_root():
+    return {"aloha": "World"}
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
