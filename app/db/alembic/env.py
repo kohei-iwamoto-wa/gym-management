aa5c79d709b01b4c74e3d401822d1743e2d86ebf
iwamoto.kohei <kohei.iwamoto@persol.co.jp>
2025-05-20 17:57:33 +0900

    alenbic

--- app/db/alembic/env.py
+++ app/db/alembic/env.py
@@ -13,12 +13,25 @@ config = context.config
 fileConfig(config.config_file_name)
 
 # 環境変数から DB URL を取得して、alembic.ini の設定を上書き
-config.set_main_option('sqlalchemy.url', 'mysql+pymysql://addmin_user:admin@local-aurora-mysql:3306/gym_db')
+config.set_main_option('sqlalchemy.url', 'mysql+pymysql://admin:admin@local-aurora-mysql:3306/gym_db')
 
 # モデルの Base を import
 import sys
 import os
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-from models import Base
+sys.path.append(os.path.dirname(__file__))
+
+print("現在のカレントディレクトリ")
+print(os.getcwd())
+print("---------------------------------")
+
+files = os.listdir()
+print("現在のディレクトリ内のファイル一覧:")
+for f in files:
+    print(f)
+print("---------------------------------")
+
+from models import Base, User, Bukken, Kukaku
 
 target_metadata = Base.metadata
+
+print("target_metadata", target_metadata)
